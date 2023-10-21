f06456908faafbc29ff09de1ec174c2edc324922
alfandi2020 <pandibl4530@gmail.com>
2023-10-21 08:49:23 +0700

    a

--- application/views/body/sidebar.php
+++ application/views/body/sidebar.php
@@ -64,7 +64,7 @@
                       <li><a href="<?= base_url('barang/satuan') ?>">Satuan</a></li>
                       <li><a href="<?= base_url('barang/kategori') ?>">Kategori</a></li>
                       <li><a href="<?= base_url('barang/brand') ?>">brand</a></li>
-                      <li><a href="dashboard-04.html">Role</a></li>
+                      <!-- <li><a href="dashboard-04.html">Role</a></li> -->
                     </ul>
                   </li>
                   <!---- Pembelian--->
