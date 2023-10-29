415946336d19c5dfded9a63546bc010e267f4590
alfandi2020 <pandibl4530@gmail.com>
2023-10-30 00:34:56 +0700

    aa

--- application/views/pos/sale.php
+++ application/views/pos/sale.php
@@ -1108,7 +1108,12 @@
                                                         var jumlah = $('.harga'+i+'').val().replace(/[^a-zA-Z0-9 ]/g, '') * j * qty_isi - diskon_item.replace(/[^a-zA-Z0-9 ]/g, '')
                                                             $('.jumlah'+i+'').val(jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
                                                             $('.stock-c'+i+'').val($('.stock'+i+'').val() - j * qty_isi);
-                                                            $('.total_pos').html("Rp."+jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
+                                                            var total_pos_fix = 0;
+                                                            for (let t = 1; t <=counter; t++) {
+                                                              total_pos_fix += parseInt($(".jumlah"+t+"")[0].value.replace(/[^a-zA-Z0-9 ]/g, ''))
+                                                            }
+                                                            $('.total_pos').html("Rp."+total_pos_fix.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
+
 
                                                 }
 
@@ -1123,7 +1128,11 @@
                                             var jumlah = $('.harga'+i+'').val().replace(/[^a-zA-Z0-9 ]/g, '') * qty * qty_isi_satuan - j.replace(/[^a-zA-Z0-9 ]/g, '')
                                             $('.jumlah'+i+'').val(jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
                                             $('.stock-c'+i+'').val($('.stock'+i+'').val() - qty * qty_isi_satuan);
-                                            $('.total_pos').html("Rp."+jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
+                                            var total_pos_fix = 0;
+                                             for (let t = 1; t <=counter; t++) {
+                                               total_pos_fix += parseInt($(".jumlah"+t+"")[0].value.replace(/[^a-zA-Z0-9 ]/g, ''))
+                                            }
+                                             $('.total_pos').html("Rp."+total_pos_fix.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
                             })
                             satuan_x.change(function(){
                                         var id=$(this).val();
@@ -1135,7 +1144,11 @@
                                                     console.log(jumlah)
                                                     $('.jumlah'+i+'').val(jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
                                                     $('.stock-c'+i+'').val($('.stock'+i+'').val() - qty * j);
-                                                    $('.total_pos').html("Rp."+jumlah.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
+                                                    var total_pos_fix = 0;
+                                                     for (let t = 1; t <=counter; t++) {
+                                                      total_pos_fix += parseInt($(".jumlah"+t+"")[0].value.replace(/[^a-zA-Z0-9 ]/g, ''))
+                                                    }
+                                                    $('.total_pos').html("Rp."+total_pos_fix.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1."));
 
                             })
                         }
