151dde88f06c0481b06cf1289f107bec72c6a55f
adpratama <adpratama.adp@gmail.com>
2024-01-15 08:05:53 +0700

    perbaiki bug modal koreksi dan mutasi barang

--- application/views/inventori/histori_koreksi.php
+++ application/views/inventori/histori_koreksi.php
@@ -99,66 +99,6 @@
         </div>
     </div>
 </div>
-<div class="modal fade" id="buatStockkoreksi" tabindex="-1" role="dialog" aria-labelledby="buatStockkoreksi" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="buatStockkoreksiLongTitle">Buat Koreksi</h5>
-                <button class="btn-close py-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="<?= base_url('inventori/add_koreksi') ?>" method="post" class="form theme-form dark-inputs" id="barangForm">
-                <div class="modal-body">
-                    <div class="row">
-                        <div class="mb-3">
-                            <input type="hidden" name="no_urut" id="no_urut" class="form-control" value="<?= $no_urut ?>">
-                            <label for="No. Koreksi" class="form-label">No. Koreksi</label>
-                            <input type="text" class="form-control" name="no_koreksi" id="no_koreksi" value="<?= $no_koreksi ?>" readonly>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="mb-3">
-                            <label class="form-label" for="Tanggal">Tanggal</label>
-                            <input class="form-control" id="tanggal" name="tanggal" type="date" value="<?= date('Y-m-d') ?>" required>
-                        </div>
-                    </div>
-                    <div class="row">
-
-                        <div class="mb-3">
-                            <label class="form-label" for="Tanggal">Lokasi barang</label>
-                            <select name="gudang" id="gudang" class="form-select input-air-primary digits" onchange="getBarang()" required>
-                                <option value="">--</option>
-                                <?php
-                                foreach ($gudang2 as $g) {
-                                ?>
-                                    <option value="<?= $g->id ?>">(<?= $g->kode ?>) <?= $g->nama ?></option>
-                                <?php
-                                }
-                                ?>
-                            </select>
-                        </div>
-                    </div>
-                    <div class="col-12">
-                        <div class="mb-3">
-                            <label for="Keterangan" class="form-label">Keterangan</label>
-                            <textarea name="keterangan" id="keterangan" cols="30" rows="3" class="form-control"></textarea>
-                        </div>
-                    </div>
-                </div>
-                <div class="modal-footer">
-
-                    <div class="row">
-                        <div class="col-12 text-end">
-                            <div class="">
-                                <label for="submit" class="form-label">&nbsp;</label>
-                                <button type="submit" class="btn btn-primary" name="submit" data-bs-toggle="tooltip" title="Simpan" value="proses">Buat</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
--- application/views/inventori/mutasi_barang.php
+++ application/views/inventori/mutasi_barang.php
@@ -34,3 +34,80 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="entri_barang" tabindex="-1" role="dialog" aria-labelledby="entri_barang" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="entri_barangLongTitle">Entri barang </h5>
+                <button class="btn-close py-0" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="<?= base_url('inventori/add_mutasi') ?>" method="post" class="form theme-form dark-input" id="myForm">
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col">
+                            <input type="hidden" name="no_urut" id="no_urut" class="form-control" value="<?= $no_urut ?>">
+                            <label for="category" class="form-label">No. Mutasi barang</label>
+                            <div class="input-group">
+                                <input type="text" class="form-control form-control-sm nominal" name="no_mutasi" id="no_mutasi" value="<?= $no_mutasi ?>" readonly autofocus>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col">
+                            <label for="category" class="form-label">Tanggal</label>
+                            <div class="input-group">
+                                <input class="form-control" id="tanggal" name="tanggal" type="date" value="<?= date('Y-m-d') ?>" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col">
+                            <label for="category" class="form-label">Lokasi asal</label>
+                            <div class="input-group">
+                                <select name="lokasi_asal" id="lokasi_asal" class="form-select">
+                                    <option value="">--Pilih lokasi barang</option>
+                                    <?php
+                                    foreach ($gudang2 as $g) {
+                                    ?>
+                                        <option value="<?= $g->id ?>">(<?= $g->kode ?>) <?= $g->nama ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col">
+                            <label for="category" class="form-label">Lokasi tujuan</label>
+                            <div class="input-group">
+                                <select name="lokasi_tujuan" id="lokasi_tujuan" class="form-select">
+                                    <option value="">--Pilih lokasi barang</option>
+                                    <?php
+                                    foreach ($gudang2 as $g) {
+                                    ?>
+                                        <option value="<?= $g->id ?>">(<?= $g->kode ?>) <?= $g->nama ?></option>
+                                    <?php
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-3">
+                        <div class="col">
+                            <label for="category" class="form-label">Keterangan</label>
+                            <div class="input-group">
+                                <textarea name="keterangan" id="keterangan" cols="30" rows="5" class="form-control"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
